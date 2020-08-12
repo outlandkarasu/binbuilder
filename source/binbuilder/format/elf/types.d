ab92565458857b6e58a7de4af6daea2722764ceb
outland.karasu <outland.karasu@gmail.com>
2020-08-12 22:58:25 +0900

    add bits enum.

--- source/binbuilder/format/elf/types.d
+++ source/binbuilder/format/elf/types.d
@@ -2,3 +2,20 @@
 Basic types module.
 */
 module binbuilder.format.elf.types;
+
+/**
+Binary architecture bits.
+*/
+enum Bits : uint
+{
+    /**
+    Architecture bits 32.
+    */
+    bits32 = 32,
+
+    /**
+    Architecture bits 64.
+    */
+    bits64 = 64,
+}
+
