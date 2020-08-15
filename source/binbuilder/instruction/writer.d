ea9f0e3f47569b291001099753c7724a41ccc1bf
outland.karasu <outland.karasu@gmail.com>
2020-08-15 10:21:33 +0900

    add simple writer for test.

--- source/binbuilder/instruction/writer.d
+++ source/binbuilder/instruction/writer.d
@@ -3,6 +3,8 @@ Instruction writer.
 */
 module binbuilder.instruction.writer;
 
+import std.container : Array;
+
 /**
 Instruction writer.
 */
@@ -43,5 +45,47 @@ interface Writeable
         Writing bytes length.
     */
     @property size_t length() const @nogc nothrow pure @safe scope;
+}
+
+/**
+Array writer.
+*/
+class ArrayWriter : Writer
+{
+    override void write(ubyte value) @nogc nothrow pure @safe scope
+    {
+        (() @trusted => array_ ~= value)();
+    }
+
+    override void write(scope const(ubyte)[] bytes) @nogc nothrow pure @safe scope
+    {
+        (() @trusted => array_ ~= bytes)();
+    }
+
+    ubyte opIndex(size_t i) const @nogc nothrow pure @safe scope
+    {
+        return array_[i];
+    }
+
+private:
+    Array!ubyte array_;
+}
+
+///
+nothrow pure @safe unittest
+{
+    auto writer = new ArrayWriter();
+
+    writer.write(0x01);
+    writer.write(0x02);
+    writer.write(0x03);
+    assert(writer[0] == 0x01);
+    assert(writer[1] == 0x02);
+    assert(writer[2] == 0x03);
+
+    writer.write([0x04, 0x05, 0x06]);
+    assert(writer[3] == 0x04);
+    assert(writer[4] == 0x05);
+    assert(writer[5] == 0x06);
 }
 
